9d9843c7f7539fc787a426fc7df04eb317393202
Lucas William <lucaswilliameufrasio@gmail.com>
2020-10-03 22:15:58 -0300

    feat: ensure DbAddAccount return null if LoadAccountByEmailRepository not return null

--- backend/src/data/test/mock-db-account.ts
+++ backend/src/data/test/mock-db-account.ts
@@ -1,4 +1,5 @@
 import { AddAccountRepository } from '@/data/protocols/db/account/add-account-repository'
+import { LoadAccountByEmailRepository } from '@/data/protocols/db/account/load-account-by-email-repository'
 import { AccountModel } from '@/domain/models/account'
 import { AddAccountParams } from '@/domain/usecases/account/add-account'
 import { mockAccountModel } from '@/domain/test'
@@ -9,6 +10,16 @@ export class AddAccountRepositorySpy implements AddAccountRepository {
 
   async add (data: AddAccountParams): Promise<AccountModel> {
     this.addAccountParams = data
+    return this.accountModel
+  }
+}
+
+export class LoadAccountByEmailRepositorySpy implements LoadAccountByEmailRepository {
+  accountModel = mockAccountModel()
+  email: string
+
+  async loadByEmail (email: string): Promise<AccountModel> {
+    this.email = email
     return this.accountModel
   }
 }
--- backend/src/data/usecases/account/add-account/db-add-account.ts
+++ backend/src/data/usecases/account/add-account/db-add-account.ts
@@ -1,17 +1,25 @@
 import { Hasher } from '@/data/protocols/cryptography/hasher'
 import { AddAccountRepository } from '@/data/protocols/db/account/add-account-repository'
+import { LoadAccountByEmailRepository } from '@/data/protocols/db/account/load-account-by-email-repository'
 import { AccountModel } from '@/domain/models/account'
 import { AddAccount, AddAccountParams } from '@/domain/usecases/account/add-account'
 
 export class DbAddAccount implements AddAccount {
   constructor (
     private readonly hasher: Hasher,
-    private readonly addAccount: AddAccountRepository
+    private readonly addAccountRepository: AddAccountRepository,
+    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
   ) {}
 
-  async add (account: AddAccountParams): Promise<AccountModel> {
-    const hashedPassword = await this.hasher.hash(account.password)
-    const newAccount = await this.addAccount.add((Object.assign({}, account, { password: hashedPassword })))
-    return newAccount
+  async add (accountData: AddAccountParams): Promise<AccountModel> {
+    const account = await this.loadAccountByEmailRepository.loadByEmail(accountData.email)
+
+    if (!account) {
+      const hashedPassword = await this.hasher.hash(accountData.password)
+      const newAccount = await this.addAccountRepository.add((Object.assign({}, accountData, { password: hashedPassword })))
+      return newAccount
+    }
+
+    return null
   }
 }
