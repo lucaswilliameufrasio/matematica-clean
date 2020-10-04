cda65567ca204958b0f602fdfcbd1e1a2f1ad15d
Lucas William <lucaswilliameufrasio@gmail.com>
2020-10-03 21:12:05 -0300

    test: ensure DbAddAccount calls Hasher with correct value

---
+++ backend/src/data/usecases/account/add-account/db-add-account.spec.ts
@@ -0,0 +1,17 @@
+import { HasherSpy } from '@/data/test/mock-cryptography'
+import { DbAddAccount } from './db-add-account'
+
+describe('DbAddAccount UseCase', () => {
+  test('Should call Hasher with correct password', async () => {
+    const hasherSpy = new HasherSpy()
+    const sut = new DbAddAccount(hasherSpy)
+    const accountParams = {
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    }
+    await sut.add(accountParams)
+
+    expect(hasherSpy.plaintext).toBe(accountParams.password)
+  })
+})
