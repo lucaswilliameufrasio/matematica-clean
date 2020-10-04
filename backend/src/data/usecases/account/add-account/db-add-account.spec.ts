4d9df7dbea453c80271ab47945e79cd2be627e25
Lucas William <lucaswilliameufrasio@gmail.com>
2020-10-03 21:21:18 -0300

    refactor: move sut creation to a factory method

--- backend/src/data/usecases/account/add-account/db-add-account.spec.ts
+++ backend/src/data/usecases/account/add-account/db-add-account.spec.ts
@@ -2,10 +2,24 @@ import { HasherSpy } from '@/data/test'
 import { mockAccountModel } from '@/domain/test'
 import { DbAddAccount } from './db-add-account'
 
+type SutTypes = {
+  sut: DbAddAccount
+  hasherSpy: HasherSpy
+}
+
+const makeSut = (): SutTypes => {
+  const hasherSpy = new HasherSpy()
+  const sut = new DbAddAccount(hasherSpy)
+
+  return {
+    sut,
+    hasherSpy
+  }
+}
+
 describe('DbAddAccount UseCase', () => {
   test('Should call Hasher with correct password', async () => {
-    const hasherSpy = new HasherSpy()
-    const sut = new DbAddAccount(hasherSpy)
+    const { sut, hasherSpy } = makeSut()
     const accountParams = mockAccountModel()
     await sut.add(accountParams)
 
